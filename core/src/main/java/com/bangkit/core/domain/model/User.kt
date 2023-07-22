5187606a1288270d34ccb553e7c9a3b17688c76a
daffaasha <aadaffa41@gmail.com>
2023-07-22 14:28:08 +0700

    update: User Domain
    Remove the user ID as it is not gonna be used

--- core/src/main/java/com/bangkit/core/domain/model/User.kt
+++ core/src/main/java/com/bangkit/core/domain/model/User.kt
@@ -1,8 +1,9 @@
 package com.bangkit.core.domain.model
 
 data class User(
-    val name: String,
+    val name: String? = null,
     val username: String,
-    val followers: Int,
-    val following: Int
+    val followers: Int? = null,
+    val following: Int? = null,
+    val avatar: String,
 )
