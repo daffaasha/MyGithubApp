dab2cebb348ef0f84168f217a79efeb5fccc7fd3
daffaasha <aadaffa41@gmail.com>
2023-07-24 17:23:17 +0700

    add: viewmodels for each activity

---
+++ app/src/main/java/com/bangkit/mygithubapp/detail/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.bangkit.mygithubapp.detail
+
+import androidx.lifecycle.ViewModel
+import com.bangkit.core.domain.usecase.UserUseCase
+
+class DetailViewModel(useCase: UserUseCase) : ViewModel() {
+}
---
+++ app/src/main/java/com/bangkit/mygithubapp/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.bangkit.mygithubapp.home
+
+import androidx.lifecycle.ViewModel
+import com.bangkit.core.domain.usecase.UserUseCase
+
+class HomeViewModel(useCase: UserUseCase) : ViewModel() {
+}
