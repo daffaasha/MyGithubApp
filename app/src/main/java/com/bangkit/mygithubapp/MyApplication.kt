8577af089a7ec315100d7c570815d0fb2d7d501c
daffaasha <aadaffa41@gmail.com>
2023-07-24 17:25:06 +0700

    add: AppModule and MyApplication for Koin implementation
    contents the module for the UseCase class and the viewmodels

---
+++ app/src/main/java/com/bangkit/mygithubapp/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.bangkit.mygithubapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyApplication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/bangkit/mygithubapp/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.bangkit.mygithubapp.di
+
+import com.bangkit.core.domain.usecase.UserInteractor
+import com.bangkit.core.domain.usecase.UserUseCase
+import com.bangkit.mygithubapp.detail.DetailViewModel
+import com.bangkit.mygithubapp.favorite.FavoriteViewModel
+import com.bangkit.mygithubapp.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<UserUseCase> { UserInteractor(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { HomeViewModel(get()) }
+    viewModel { DetailViewModel(get()) }
+    viewModel { FavoriteViewModel(get()) }
+}
