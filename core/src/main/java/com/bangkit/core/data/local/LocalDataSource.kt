178d49444ed7933a06d626eb6ed8f3c32cf50af8
daffaasha <aadaffa41@gmail.com>
2023-07-24 15:28:54 +0700

    add: LocalDataSource
    Local data source class to handle the data from room database

---
+++ core/src/main/java/com/bangkit/core/data/local/LocalDataSource.kt
@@ -0,0 +1,15 @@
+package com.bangkit.core.data.local
+
+import com.bangkit.core.data.local.entity.FavoriteEntity
+import com.bangkit.core.data.local.room.FavoriteDao
+import kotlinx.coroutines.flow.Flow
+
+class LocalDataSource(private val favoriteDao: FavoriteDao) {
+
+    fun getAllFavorite(): Flow<List<FavoriteEntity>> = favoriteDao.getAllFavorite()
+
+    fun deleteFavorite(favorite: FavoriteEntity) = favoriteDao.deleteFavorite(favorite)
+
+    fun insertFavorite(favorite: FavoriteEntity) = favoriteDao.insertFavorite(favorite)
+
+}
