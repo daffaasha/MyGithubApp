ae1ace2f67f0dd123a50f47a50da93d731558f20
daffaasha <aadaffa41@gmail.com>
2023-07-24 15:22:23 +0700

    update: fix database class implementation
    forgot to add inheritance to the RoomDatabase class

--- core/src/main/java/com/bangkit/core/data/local/room/FavoriteDatabase.kt
+++ core/src/main/java/com/bangkit/core/data/local/room/FavoriteDatabase.kt
@@ -1,10 +1,11 @@
 package com.bangkit.core.data.local.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.bangkit.core.data.local.entity.FavoriteEntity
 
 @Database(entities = [FavoriteEntity::class], version = 1, exportSchema = false)
-abstract class FavoriteDatabase {
+abstract class FavoriteDatabase: RoomDatabase() {
 
     abstract fun favoriteDao():FavoriteDao
 
