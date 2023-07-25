c90816bc7bece33f0c4e5d6461f0760751fda70a
daffaasha <aadaffa41@gmail.com>
2023-07-25 20:33:38 +0700

    update: MyApplication wasn't written properly
    add the Koin implementation in the class

--- app/src/main/java/com/bangkit/mygithubapp/MyApplication.kt
+++ app/src/main/java/com/bangkit/mygithubapp/MyApplication.kt
@@ -1,11 +1,31 @@
 package com.bangkit.mygithubapp
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.app.Application
+import com.bangkit.core.di.databaseModule
+import com.bangkit.core.di.networkModule
+import com.bangkit.core.di.repositoryModule
+import com.bangkit.mygithubapp.di.useCaseModule
+import com.bangkit.mygithubapp.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-class MyApplication : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+open class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    databaseModule,
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
+                )
+            )
+        }
     }
 }
