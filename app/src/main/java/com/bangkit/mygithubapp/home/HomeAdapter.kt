37e617dd091f2096b0ad30b6001162b0b34b39d1
daffaasha <aadaffa41@gmail.com>
2023-07-25 20:39:57 +0700

    add: adapter and it's item layout
    adding the adapter and the item layout for home activity recycler view

---
+++ app/src/main/java/com/bangkit/mygithubapp/home/HomeAdapter.kt
@@ -0,0 +1,37 @@
+package com.bangkit.mygithubapp.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.core.domain.model.User
+import com.bangkit.mygithubapp.databinding.UserItemBinding
+import com.bangkit.mygithubapp.util.loadImage
+
+class HomeAdapter(
+    private val listUser: List<User>,
+    private val onClick: (User) -> Unit
+) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
+    class HomeViewHolder(var binding : UserItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeViewHolder(binding)
+    }
+
+    override fun getItemCount() = listUser.size
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        val avatar = listUser[position].avatar
+        val username = listUser[position].username
+
+        with(holder.binding) {
+            tvUsername.text = username
+            ivAvatar.loadImage(avatar)
+        }
+
+        holder.itemView.setOnClickListener {
+            onClick(listUser[position])
+        }
+    }
+}
