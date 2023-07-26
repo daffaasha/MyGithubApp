63f9bd1e127e07f5f9492bed5c4b3435875b8d6d
daffaasha <aadaffa41@gmail.com>
2023-07-26 15:31:45 +0700

    add: UserFollowAdapter for pager in detail
    creating adapter for showing the list of followers/following in the detail activity view pager for showing the list

---
+++ app/src/main/java/com/bangkit/mygithubapp/detail/UserFollowAdapter.kt
@@ -0,0 +1,32 @@
+package com.bangkit.mygithubapp.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.core.domain.model.User
+import com.bangkit.mygithubapp.databinding.UserItemBinding
+import com.bangkit.mygithubapp.util.loadImage
+
+class UserFollowAdapter(
+    private val listFollow: List<User>
+) : RecyclerView.Adapter<UserFollowAdapter.DetailViewHolder>() {
+
+    class DetailViewHolder(var binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
+        val binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DetailViewHolder(binding)
+    }
+
+    override fun getItemCount() = listFollow.size
+
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
+        val avatar = listFollow[position].avatar
+        val username = listFollow[position].username
+
+        with(holder.binding) {
+            tvUsername.text = username
+            ivAvatar.loadImage(avatar)
+        }
+    }
+}
