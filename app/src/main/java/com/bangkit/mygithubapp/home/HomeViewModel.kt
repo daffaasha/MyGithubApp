15cd29db44a2a6588c35575ccaae339b71cf6522
daffaasha <aadaffa41@gmail.com>
2023-07-26 12:37:47 +0700

    update: implement the list of users in HomeActivity
    implement all of the functions needed by the user, like the
    - recycler view
    - observers from the viewmodel
    - the search user username

--- app/src/main/java/com/bangkit/mygithubapp/home/HomeViewModel.kt
+++ app/src/main/java/com/bangkit/mygithubapp/home/HomeViewModel.kt
@@ -1,7 +1,24 @@
 package com.bangkit.mygithubapp.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bangkit.core.data.Result
+import com.bangkit.core.domain.model.User
 import com.bangkit.core.domain.usecase.UserUseCase
+import kotlinx.coroutines.launch
 
-class HomeViewModel(useCase: UserUseCase) : ViewModel() {
+class HomeViewModel(private val useCase: UserUseCase) : ViewModel() {
+
+    private val _listUser = MutableLiveData<Result<List<User>>>()
+    val listUser: LiveData<Result<List<User>>> = _listUser
+
+    fun getSearchUser(username: String)  {
+        viewModelScope.launch {
+            useCase.getUserByUsername(username).collect { result ->
+                _listUser.value = result
+            }
+        }
+    }
 }
--- app/src/main/java/com/bangkit/mygithubapp/home/MainActivity.kt
+++ app/src/main/java/com/bangkit/mygithubapp/home/MainActivity.kt
@@ -1,8 +1,22 @@
 package com.bangkit.mygithubapp.home
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bangkit.core.data.Result
+import com.bangkit.core.domain.model.User
+import com.bangkit.mygithubapp.R
 import com.bangkit.mygithubapp.databinding.ActivityMainBinding
+import com.bangkit.mygithubapp.detail.DetailActivity
+import com.bangkit.mygithubapp.favorite.FavoriteActivity
+import com.bangkit.mygithubapp.util.showToast
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +28,85 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.listUser.observe(this) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    showLoading(true)
+                }
+                is Result.Success -> {
+                    showLoading(false)
+                    ifListEmpty(result.data.isEmpty())
+                    setupRecyclerView(result.data)
+                }
+                is Result.Error -> {
+                    showLoading(false)
+                    showToast(result.error, this)
+                }
+            }
+        }
+    }
+    private fun setupRecyclerView(listUser: List<User>) {
+        binding.rvUser.layoutManager = LinearLayoutManager(this)
+        val adapter = HomeAdapter(listUser) {
+            val intent = Intent(this@MainActivity, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.EXTRA_USERNAME, it.username)
+            startActivity(intent)
+        }
+        binding.rvUser.adapter = adapter
+    }
+
+    private fun showLoading(show: Boolean) {
+        binding.pgLoading.visibility = if (show) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+
+    private fun ifListEmpty(isEmpty: Boolean) {
+        binding.tvEmptyList.visibility = if (isEmpty) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.search)?.actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = "Input Username"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                viewModel.getSearchUser(query)
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.favorite -> {
+                val intent = Intent(this, FavoriteActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
