f90e42450b20281b7536d789fa01a215b2fd2777
daffaasha <aadaffa41@gmail.com>
2023-07-26 11:27:46 +0700

    Update Result.kt
    change the form of the result class to the more familiar one

--- core/src/main/java/com/bangkit/core/data/Result.kt
+++ core/src/main/java/com/bangkit/core/data/Result.kt
@@ -1,7 +1,7 @@
 package com.bangkit.core.data
 
-sealed class Result<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Result<T>(data)
-    class Loading<T>(data: T? = null) : Result<T>(data)
-    class Error<T>(message: String, data: T? = null) : Result<T>(data, message)
+sealed class Result<out R> private constructor() {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val error: String) : Result<Nothing>()
+    object Loading : Result<Nothing>()
 }
