247fe5bb1646291b20726753fe2204b5691c1171
daffaasha <aadaffa41@gmail.com>
2023-07-27 15:33:31 +0700

    add: implement the showing follower/ing
    using view pager, the 2 frgaments tasked to show the follower/ing

--- app/src/main/java/com/bangkit/mygithubapp/detail/followfragment/FollowingFragment.kt
+++ app/src/main/java/com/bangkit/mygithubapp/detail/followfragment/FollowingFragment.kt
@@ -5,19 +5,71 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bangkit.core.data.Result
+import com.bangkit.core.domain.model.User
 import com.bangkit.mygithubapp.R
+import com.bangkit.mygithubapp.databinding.FragmentFollowBinding
+import com.bangkit.mygithubapp.detail.DetailViewModel
+import com.bangkit.mygithubapp.detail.UserFollowAdapter
+import com.bangkit.mygithubapp.util.showToast
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 class FollowingFragment : Fragment() {
+
+    private lateinit var binding: FragmentFollowBinding
+    private val viewModel by activityViewModel<DetailViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_following, container, false)
+        binding = FragmentFollowBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        const val ARG_USERNAME = "arg_username"
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        viewModel.userFollowing.observe(viewLifecycleOwner) { result ->
+            when(result) {
+                is Result.Loading -> showLoading(true)
+                is Result.Success -> {
+                    showLoading(false)
+                    setupRecyclerView(result.data)
+                }
+                is Result.Error -> {
+                    showLoading(false)
+                    showToast(result.error, requireContext())
+                }
+            }
+        }
+    }
+
+    private fun setupRecyclerView(listUser: List<User>) {
+        ifListEmpty(listUser.isEmpty())
+        binding.rvUser.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvUser.adapter = UserFollowAdapter(listUser)
+    }
+
+    private fun ifListEmpty(isEmpty: Boolean) {
+        binding.tvEmptyList.text = getString(R.string.empty_following)
+        binding.tvEmptyList.visibility = if (isEmpty) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+
+    private fun showLoading(showLoading: Boolean) {
+        binding.pgLoading.visibility = if (showLoading) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
     }
 }
