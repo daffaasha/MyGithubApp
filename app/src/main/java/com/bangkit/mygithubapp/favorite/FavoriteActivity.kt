943e73ff5ec5023fc6bb95d24824d5bb73d0d457
daffaasha <aadaffa41@gmail.com>
2023-07-27 18:40:23 +0700

    add: implementation to the favorite list user
    add the functions to the viewmodel
    layouting the activity
    and lastly logic implementation in the activity

--- app/src/main/java/com/bangkit/mygithubapp/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/bangkit/mygithubapp/favorite/FavoriteActivity.kt
@@ -1,12 +1,52 @@
 package com.bangkit.mygithubapp.favorite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bangkit.mygithubapp.R
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bangkit.mygithubapp.databinding.ActivityFavoriteBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.bangkit.core.domain.model.User
+import com.bangkit.mygithubapp.adapter.UserListAdapter
+import com.bangkit.mygithubapp.detail.DetailActivity
 
 class FavoriteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFavoriteBinding
+    private val viewModel: FavoriteViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favorite)
+        binding = ActivityFavoriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        viewModel.getAllFavorite()
+        viewModel.favoriteUser.observe(this) { listUser ->
+            ifListEmpty(listUser.isEmpty())
+            setupRecyclerView(listUser)
+        }
+    }
+
+    private fun ifListEmpty(isEmpty: Boolean) {
+        binding.tvEmptyList.visibility = if (isEmpty) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+
+    private fun setupRecyclerView(listUser: List<User>) {
+        binding.rvUser.layoutManager = LinearLayoutManager(this)
+        val adapter = UserListAdapter(listUser) {
+            val intent = Intent(this@FavoriteActivity, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.EXTRA_USERNAME, it.username)
+            startActivity(intent)
+        }
+        binding.rvUser.adapter = adapter
     }
 }
--- app/src/main/java/com/bangkit/mygithubapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/bangkit/mygithubapp/favorite/FavoriteViewModel.kt
@@ -1,7 +1,28 @@
 package com.bangkit.mygithubapp.favorite
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bangkit.core.domain.model.User
 import com.bangkit.core.domain.usecase.UserUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class FavoriteViewModel(useCase: UserUseCase) : ViewModel() {
+class FavoriteViewModel(private val useCase: UserUseCase) : ViewModel() {
+
+
+    private val _favoriteUser = MutableLiveData<List<User>>()
+    val favoriteUser: LiveData<List<User>> = _favoriteUser
+
+    fun getAllFavorite() {
+        viewModelScope.launch {
+            useCase.getAllFavoriteUser().collect {
+                _favoriteUser.value = it
+            }
+        }
+    }
+
 }
