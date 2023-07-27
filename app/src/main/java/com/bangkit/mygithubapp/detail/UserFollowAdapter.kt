bbeca675663788471126fbd92c6d4e617d705739
daffaasha <aadaffa41@gmail.com>
2023-07-27 15:22:01 +0700

    Update UserFollowAdapter.kt
    - change the layout to the user follow insetad of the usual user
    - and change the image instead of square, i used circle

--- app/src/main/java/com/bangkit/mygithubapp/detail/UserFollowAdapter.kt
+++ app/src/main/java/com/bangkit/mygithubapp/detail/UserFollowAdapter.kt
@@ -4,17 +4,19 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bangkit.core.domain.model.User
+import com.bangkit.mygithubapp.databinding.FollowUserItemBinding
 import com.bangkit.mygithubapp.databinding.UserItemBinding
+import com.bangkit.mygithubapp.util.loadCircleImage
 import com.bangkit.mygithubapp.util.loadImage
 
 class UserFollowAdapter(
     private val listFollow: List<User>
 ) : RecyclerView.Adapter<UserFollowAdapter.DetailViewHolder>() {
 
-    class DetailViewHolder(var binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root)
+    class DetailViewHolder(var binding: FollowUserItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
-        val binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = FollowUserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return DetailViewHolder(binding)
     }
 
@@ -26,7 +28,7 @@ class UserFollowAdapter(
 
         with(holder.binding) {
             tvUsername.text = username
-            ivAvatar.loadImage(avatar)
+            ivAvatar.loadCircleImage(avatar)
         }
     }
 }
