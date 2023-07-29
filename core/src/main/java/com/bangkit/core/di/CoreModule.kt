a28e6b1c7da903cab8ad87c38e943762ee33a754
daffaasha <aadaffa41@gmail.com>
2023-07-29 18:39:12 +0700

    add: Certification Pinning in the CoreModule

--- core/src/main/java/com/bangkit/core/di/CoreModule.kt
+++ core/src/main/java/com/bangkit/core/di/CoreModule.kt
@@ -9,6 +9,7 @@ import com.bangkit.core.data.remote.network.ApiService
 import com.bangkit.core.domain.repository.IUserRepository
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -33,6 +34,12 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.github.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+            .add(hostname, "sha256/e0IRz5Tio3GA1Xs4fUVWmH1xHDiH2dMbVtCBSkOIdqM=")
+            .add(hostname, "sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=")
+            .build()
         val authInterceptor = Interceptor { chain ->
             val req = chain.request()
             val requestHeaders = req.newBuilder()
@@ -43,6 +50,7 @@ val networkModule = module {
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .addInterceptor(authInterceptor)
+            .certificatePinner(certificatePinner)
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
             .build()
